d6ef8948eeac71dd0a6e285d901afebdda149916
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2026-07-16 02:27:33 -0600

    test

--- cicd_test.sh
+++ cicd_test.sh
@@ -1,7 +1,7 @@
-git tag -d v0.1.0-rc3
-git push origin --delete v0.1.0-rc3
+git tag -d v0.1.0-rc4
+git push origin --delete v0.1.0-rc4
 git add .
 git commit -m "test"
 git push
-git tag v0.1.0-rc3
-git push origin v0.1.0-rc3
+git tag v0.1.0-rc4
+git push origin v0.1.0-rc4
